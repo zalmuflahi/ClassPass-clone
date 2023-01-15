c4c1a24fd87c30c6900a29fd3671a879b4f7d8bd
Larson Collier <collierlarson@gmail.com>
2023-01-15 03:13:20 -0500

    added hover and crop to photo

--- client/src/components/BusinessPage.jsx
+++ client/src/components/BusinessPage.jsx
@@ -3,10 +3,12 @@ const BusinessPage = () => {
     return(
         <div className="mainComponents">
             <div className="p-4 mx-10 flex space-x-4 w-4/5 flex justify-center h-screen">
-                <div className="flex-block w-1/2">
-                    <img className="w-full" src="https://classpass-res.cloudinary.com/image/upload/f_auto/q_auto/psh7k9ugxtuvhviozoxw.jpg"></img>
+                <div id="left side column" className="flex-block w-1/2">
+                    <div id="image container" className="aspect-[5/3] overflow-hidden rounded-md">
+                        <img className=" hover:scale-105 transition ease-in-out" src="https://classpass-res.cloudinary.com/image/upload/f_auto/q_auto/psh7k9ugxtuvhviozoxw.jpg"></img>
+                    </div>
                 </div>
-                <div className="flex-block w-1/4">
+                <div id="right side column" className="flex-block w-1/4">
                     <img src="https://i.insider.com/5c1bd9ea630d9b259d4df48c?width=1000&format=jpeg&auto=webp"></img>
                 </div>
             </div>

adf64dff2c003504ef5408d5f30303f6df90ba84
Larson Collier <collierlarson@gmail.com>
2023-01-15 06:07:38 -0500

    added stand in logo and name. Added logo to banner

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const Header = () => {
     return(
         <div className = "headerContainer">
-            <div id="navLinks">
-
-                <Link  className="links" to="/">Home</Link>
+            <img className="h-4/5 ratio-auto float-left" src="https://53.fs1.hubspotusercontent-na1.net/hubfs/53/Fergals/Case%20Study%20Logos/ClassPass%20Logo.png" />
+            <div id="navLinks" className="float-right" >
+                <Link className="links" to="/">Home</Link>
                 <Link className="links" to="/Login">Login</Link>
                 <Link className="links" to="/BusinessPage">View Business</Link>
             </div>

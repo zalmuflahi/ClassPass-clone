ee1102a70e8ba68b152f345eb30a43678aef8a07
Larson Collier <collierlarson@gmail.com>
2023-01-15 01:15:48 -0500

    installed tailwind, added notes file

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
     return(
         <div className = "headerContainer">
-            <Link className="links" to="/">HOME</Link>
+            <Link  className="links" to="/">HOME</Link>
             <Link className="links" to="/Login">LOGIN</Link>
             <Link className="links" to="/BusinessPage">BUSINESS</Link>
         </div>

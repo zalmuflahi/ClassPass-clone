e2e73fce08fa33e1c936dc3fe486d098d450f1ab
Larson Collier <collierlarson@gmail.com>
2023-01-15 12:40:53 -0500

    added google map image and .env for key storage

--- client/src/components/busPageContents/RightColumn.jsx
+++ client/src/components/busPageContents/RightColumn.jsx
@@ -1,20 +1,13 @@
-const RightColumn = () => {
-    const GAPIkey = import.meta.env.GOOGLE_API_KEY
 
+const RightColumn = () => {
+    const GAPIkey = import.meta.env.VITE_API_KEY_GAPI
+
+    
     return(
         <div id="right side column" className="flex-block w-1/4 break-all">
-            <img className="w-full" src="https://i.insider.com/5c1bd9ea630d9b259d4df48c?width=1000&format=jpeg&auto=webp"></img>
             
             <div>
-                <iframe
-                    className="w-full"
-                    style={{border:0}}
-                    loading="lazy"
-                    allowfullscreen
-                    referrerpolicy="no-referrer-when-downgrade"
-                    src={`https://www.google.com/maps/embed/v1/place?key=${GAPIkey}&q=Space+Needle,Seattle+WA`}>
-                </iframe>
-                
+                <img className="w-full rounded-md" src={`https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c%20-73.998&zoom=12&size=400x400&key=${GAPIkey}`}></img>
             </div>
             <h1 className="break-all w-full">FILLERe ofhweofh wehf wio ehfo wieofhow iehfwie ofhwiehfowe ihofihweoifhowiehfoiwheoifhwoeifhowiehfihwef</h1>
         </div>

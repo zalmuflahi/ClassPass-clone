babf8bc859ee8f3cb6d2febd426b1d0393c576cb
Larson Collier <collierlarson@gmail.com>
2023-01-18 16:16:14 -0500

    carousel onClick added

--- client/src/components/CarouselItem.jsx
+++ client/src/components/CarouselItem.jsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 const CarouselItem = ({gym}) => {
+    const navigate = useNavigate()
+
+    const clickHandler = () =>{
+        navigate(`/BusinessPage/${gym.id}`)
+    }
+
     return(
         <Carousel.Item>
             <img
                 className="d-block w-100"
                 src={gym.image}
                 alt={gym.name}
+                onClick={()=>{clickHandler()}}
             />
             <Carousel.Caption>
                 <h3>{gym.name}</h3>

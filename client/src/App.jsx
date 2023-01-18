77434c8fe2f45f308d656b0b645ca92b19a0c0b2
Larson Collier <collierlarson@gmail.com>
2023-01-18 12:01:28 -0500

    added credits in header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Login from './components/Login.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
@@ -8,13 +8,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "./App.css"
 
 function App() {
-
+  const [credits, setCredits] = useState(200)
   const [userObj, setUserObj] = useState(null)
+
+  useEffect(()=>{
+    if (userObj){
+      setCredits(200)
+    }
+    else {
+      setCredits(0)
+    }
+  },[userObj])
 
 return (
   <div className="App">
     <BrowserRouter>      
-      <Header userObj={userObj} setUserObj={setUserObj} />
+      <Header userObj={userObj} setUserObj={setUserObj} credits={credits} setCredits={setCredits}/>
         <Routes>
                 
             <Route path={'/'} element={<HomeScreen />} />

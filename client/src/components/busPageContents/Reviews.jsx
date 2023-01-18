c18f30650e6f702e33218f6e90b2658b8b84a286
John Cote <johncote61598@gmail.com>
2023-01-18 13:56:57 -0500

    slight changes to reviews componnent

--- client/src/components/busPageContents/Reviews.jsx
+++ client/src/components/busPageContents/Reviews.jsx
@@ -1,15 +1,15 @@
 const Reviews = () => {
     return(
         <div>
-            <h2>Reviews: </h2>
-            {
+            <h1>Reviews: </h1>
+            {/* {
                 reviews.map((review) => {
                     return(
                         <p>{review.content}</p>
                         //content is just a placeholder. Not sure what key is called in backend
                     )
                 })
-            }
+            } */}
         </div>
     )
 }

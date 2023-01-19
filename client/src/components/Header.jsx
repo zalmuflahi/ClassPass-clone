0b9c587fdec2078cd4cfdf038aff3c1d9e74cf32
Larson Collier <collierlarson@gmail.com>
2023-01-19 12:19:44 -0500

    readded header components that got lost

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,8 +22,8 @@ const Header = ({userObj, setUserObj, credits, setCredits}) => {
                 </Link>
             <div className="flex items-center" >
                 {userObj ? <div className="px-2"><div className="text-black font-bold font-xl">Welcome, {userObj.givenName} </div></div> : null}
-                {userObj ? <div className="px-2"><div className="bg-blue-600 font-semibold rounded-full py-2 px-3">{credits} Credits</div></div> : null}
-                {userObj ? <div className="pr-4 pl-2"><GoogleLogout  clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} /></div> : <Link className="links" to="/Login">Login</Link> }
+                {userObj ? <div className="px-2"><div className="bg-blue-600 font-semibold rounded-full py-2 px-3">{credits} Credits</div></div> : <Link className="links" to="/Login">Login</Link>}
+                {userObj ? <div className="pr-4 pl-2"><GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} /></div> : <Link to="/Login" className="px-2"><div className="bg-blue-600 font-semibold rounded-full py-2 px-3">Sign up for 200 Credits</div></Link> }
             </div>
         </div>
     )
